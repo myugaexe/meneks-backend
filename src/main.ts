cf808a9cf54b102cad7313c7449cbfbc82dbcf63
myuga <muhyugabillawalrizqi@gmail.com>
2025-06-29 13:37:32 +0700

    fix : add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+app.enableCors({
+  origin: '*',
+});
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
