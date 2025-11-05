eb8ea379a8697e206772a5ae2dd377c31fe8799d
Ardyansyah3103 <ardyansyah6859@gmail.com>
2025-11-05 23:55:29 +0700

    feat: enable cors to domain

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://ekskooola.vercel.app'],
+    credentials: true,
   });
   
   app.useGlobalPipes(
